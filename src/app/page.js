678200ff26f7685627e872075047d7fdaba29db3
Nadira Fawziyya Masnur <68457610+458-nadiraF@users.noreply.github.com>
2025-02-15 18:27:23 +0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import dynamic from 'next/dynamic';
 
 // Dynamically import the HerbOilWebsite component with no SSR
 const HerbOilWebsite = dynamic(
-  () => import('@/components/HerbOilWebsite.js').then(mod => mod.default),
+  () => import('./components/HerbOilWebsite.js').then(mod => mod.default),
   { ssr: false }
 );
 
